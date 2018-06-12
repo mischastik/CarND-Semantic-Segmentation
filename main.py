fb0dc53761f70873908b515e53fccad0ee6d1f83
Michael Balda <michael.balda@gmail.com>
2018-06-12 15:06:52 +0200

    [WIP]Implement optimize method

--- main.py
+++ main.py
@@ -33,11 +33,13 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
-    image_input = sess.graph.get_tensor_by_name(vgg_input_tensor_name)
-    keep_prob = sess.graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
-    layer3_out = sess.graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
-    layer4_out = sess.graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
-    layer7_out = sess.graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    graph = sess.graph
+    #graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
 
     return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
@@ -53,8 +55,21 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same',
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.add(output, vgg_layer7_out)
 
-    return None
+    output = tf.layers.conv2d_transpose(output, num_classes, 4, 2, padding='same',
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.add(output, vgg_layer4_out)
+
+    output = tf.layers.conv2d_transpose(output, num_classes, 16, 8, padding='same',
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.add(output, vgg_layer3_out)
+
+    return output
 tests.test_layers(layers)
 
 
@@ -67,8 +82,10 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(nn_last_layer, correct_label))
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+
+    return nn_last_layer, optimizer, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
